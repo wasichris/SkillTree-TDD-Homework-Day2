7464caf88708b80f4c74b452bc32cf421863c4b7
陳志澤 <wasi.chris@gmail.com>
2016-01-19 00:52:23 +0800

    一二三集各買了一本，價格應為100*3*0.9=270 [綠燈] - 開始重構&測試保護

---
+++ PotterShoppingCart/DiscountPackage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart
+{
+    public class DiscountPackage
+    {
+        public int PackageSize { get; set; }
+
+        public double PackageRate { get; set; }
+
+        public string BookName { get; set; }
+    }
+}
--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -8,20 +8,36 @@ namespace PotterShoppingCart
     {
         private List<Book> _books;
 
+        private List<DiscountPackage> _discountPackages;
+
+
         public ShoppingCart(List<Book> books)
         {
             this._books = books;
+            this._discountPackages = new List<DiscountPackage>
+            {
+                new DiscountPackage{BookName="Harry Potter", PackageSize = 2, PackageRate = 0.95},
+                new DiscountPackage{BookName="Harry Potter", PackageSize = 3, PackageRate = 0.9},
+            };
         }
+
+
+        public IEnumerable<string> DiscountBookNames 
+        {
+            get
+            {
+                return _discountPackages.Select(x => x.BookName).Distinct();
+            } 
+        }
+
 
         public int GetTotalPrice()
         {
-            var discountBookName = "Harry Potter";
-
             int normalPrice =
-                _books.Where(x => x.Name != discountBookName).Select(x => x.SellPrice).Sum();
+                _books.Where(x => !DiscountBookNames.Contains(x.Name)).Select(x => x.SellPrice).Sum();
 
             int discountPrice =
-                GetDiscountPrice(_books.Where(x => x.Name == discountBookName).ToList());
+                GetDiscountPrice(_books.Where(x => DiscountBookNames.Contains(x.Name)).ToList());
 
             return normalPrice + discountPrice;
         }
@@ -29,9 +45,27 @@ namespace PotterShoppingCart
         private int GetDiscountPrice(List<Book> books)
         {
             var discountPriceSum = 0;
-            var packageSize = 2;
-            var packageRate = 0.95;
 
+            foreach (var discountBookName in DiscountBookNames)
+            {
+                // loop each discount book name
+                var discountPackages = _discountPackages.Where(x => x.BookName == discountBookName);
+                var discountBooks = books.Where(x => x.Name == discountBookName).ToList();
+                foreach (var package in discountPackages.OrderByDescending(x => x.PackageSize))
+                {
+                    // loop each discount package
+                    discountPriceSum += GetPackagePrice(discountBooks, package.PackageSize, package.PackageRate);
+                }
+
+                discountPriceSum += discountBooks.Select(x => x.SellPrice).Sum();
+            }
+
+            return discountPriceSum;
+        }
+
+        private int GetPackagePrice(List<Book> books, int packageSize, double packageRate)
+        {
+            var priceSum = 0;
             var existSeries = GetSeries(books);
             while (existSeries.Count >= packageSize)
             {
@@ -43,11 +77,10 @@ namespace PotterShoppingCart
                     books.Remove(book);
                 }
 
-                discountPriceSum += Convert.ToInt16(packageOriginalPrice * packageRate);
+                priceSum += Convert.ToInt16(packageOriginalPrice * packageRate);
                 existSeries = GetSeries(books);
             }
-
-            return discountPriceSum + books.Select(x => x.SellPrice).Sum();
+            return priceSum;
         }
 
         private List<int> GetSeries(List<Book> books)
