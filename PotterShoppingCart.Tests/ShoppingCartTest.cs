16bae429b03c175a30d26a8d24420cc6d7a742b4
陳志澤 <wasi.chris@gmail.com>
2016-01-19 00:21:24 +0800

    一二三集各買了一本，價格應為100*3*0.9=270 [紅燈]

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using PotterShoppingCart;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace PotterShoppingCart.Tests
 {
@@ -45,7 +41,26 @@ namespace PotterShoppingCart.Tests
 
             // Assert
             Assert.AreEqual(expected, actual);
+        }
 
+        [TestMethod]
+        public void GetTotalPriceTest_一二三集各買了一本_價格應為270()
+        {
+            // Arrange
+            var books = new List<Book>()
+            {
+                new Book { Name="Harry Potter", Series = 1, SellPrice = 100},
+                new Book { Name="Harry Potter", Series = 2, SellPrice = 100},
+                new Book { Name="Harry Potter", Series = 3, SellPrice = 100}
+            };
+            var expected = 270;
+            var target = new ShoppingCart(books);
+
+            // Act
+            var actual = target.GetTotalPrice();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
