2bc06156f5bc4c60b79e3384302e2eae6d7b9bed
陳志澤 <wasi.chris@gmail.com>
2016-01-19 00:59:17 +0800

    第一集買了一本，第二三集各買了兩本，價格應為100*3*0.9 + 100*2*0.95 = 460 [綠燈]

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -126,5 +126,28 @@ namespace PotterShoppingCart.Tests
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void GetTotalPriceTest_第一集買了一本_第二三集各買了兩本_價格應為460()
+        {
+            // Arrange
+            var books = new List<Book>()
+            {
+                new Book { Name="Harry Potter", Series = 1, SellPrice = 100},
+                new Book { Name="Harry Potter", Series = 2, SellPrice = 100},
+                new Book { Name="Harry Potter", Series = 2, SellPrice = 100},
+                new Book { Name="Harry Potter", Series = 3, SellPrice = 100},
+                new Book { Name="Harry Potter", Series = 3, SellPrice = 100}
+            };
+            var expected = 460;
+            var target = new ShoppingCart(books);
+
+            // Act
+            var actual = target.GetTotalPrice();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+
+        }
     }
 }
