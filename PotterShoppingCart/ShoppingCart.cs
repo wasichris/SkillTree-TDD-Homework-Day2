641d34369ab6ae8f85c3b04c61fd80aced6b5444
陳志澤 <wasi.chris@gmail.com>
2016-01-19 00:20:06 +0800

    第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190 [綠燈]

--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace PotterShoppingCart
 {
@@ -16,7 +15,44 @@ namespace PotterShoppingCart
 
         public int GetTotalPrice()
         {
-            return _books.Select(x => x.SellPrice).Sum();
+            var discountBookName = "Harry Potter";
+
+            int normalPrice =
+                _books.Where(x => x.Name != discountBookName).Select(x => x.SellPrice).Sum();
+
+            int discountPrice =
+                GetDiscountPrice(_books.Where(x => x.Name == discountBookName).ToList());
+
+            return normalPrice + discountPrice;
+        }
+
+        private int GetDiscountPrice(List<Book> books)
+        {
+            var discountPriceSum = 0;
+            var packageSize = 2;
+            var packageRate = 0.95;
+
+            var existSeries = GetSeries(books);
+            while (existSeries.Count >= packageSize)
+            {
+                int packageOriginalPrice = 0;
+                for (int i = 0; i < packageSize; i++)
+                {
+                    var book = books.Where(x => x.Series == existSeries[i]).First();
+                    packageOriginalPrice += book.SellPrice;
+                    books.Remove(book);
+                }
+
+                discountPriceSum += Convert.ToInt16(packageOriginalPrice * packageRate);
+                existSeries = GetSeries(books);
+            }
+
+            return discountPriceSum + books.Select(x => x.SellPrice).Sum();
+        }
+
+        private List<int> GetSeries(List<Book> books)
+        {
+            return books.Select(x => x.Series).Distinct().ToList();
         }
     }
 }
